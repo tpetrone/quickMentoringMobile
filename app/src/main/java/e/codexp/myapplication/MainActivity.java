d5cbd0831e1081f1699d93beca6e2505cd93a30a
Igor <igor1899@hotmail.com>
2018-03-02 21:35:29 -0300

    Initial commit

---
+++ app/src/main/java/e/codexp/myapplication/MainActivity.java
@@ -0,0 +1,44 @@
+package e.codexp.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button cadastromentor = (Button) findViewById(R.id.button);
+        Button cadastromentorado = (Button) findViewById(R.id.button2);
+        Button login = (Button) findViewById(R.id.button3);
+                cadastromentor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mentor = "mentor";
+                Intent it = new Intent(MainActivity.this, SignUp.class);
+                it.putExtra("mentor", mentor);
+                startActivity(it);
+            }
+        });
+        cadastromentorado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mentorado = "mentorado";
+                Intent it = new Intent(MainActivity.this, SignUp.class);
+                it.putExtra("mentorado", mentorado);
+                startActivity(it);
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it3 = new Intent(MainActivity.this, Login.class);
+                startActivity(it3);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/e/codexp/myapplication/SignUp.java
@@ -0,0 +1,37 @@
+package e.codexp.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Intent intent = getIntent();
+        String mentor = intent.getExtras().getString("mentor");
+        String mentorado = intent.getExtras().getString("mentorado");
+        Button cadastrar = (Button) findViewById(R.id.button4);
+        if(mentor != null)
+            setTitle("Cadastrar-se como mentor");
+         if(mentorado != null)
+             setTitle("Cadastrar-se como mentorado");
+
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it4 = new Intent(SignUp.this, Login.class);
+                startActivity(it4);
+    }
+
+});
+    }}
