e8e3c70c10f5fe6f1b83920d80bce54a7ead701a
Igor <igor1899@hotmail.com>
2018-04-04 20:44:36 -0300

    WIP: PERFIL TELA

---
+++ app/src/main/java/codexp/br/senai/sp/quick_mentoring_mobile/VisualizarPerfil.java
@@ -0,0 +1,13 @@
+package codexp.br.senai.sp.quick_mentoring_mobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VisualizarPerfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visualizar_perfil);
+    }
+}
